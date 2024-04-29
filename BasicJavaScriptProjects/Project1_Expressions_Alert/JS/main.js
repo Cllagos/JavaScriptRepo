6f54c79f32577b0610825b0dd95056130b288c3d
Carlos Lagos <carloslagos85@outlook.com>
2024-04-29 14:41:50 -0500

    Updated Javascript Assignment 042924

---
+++ BasicJavaScriptProjects/Project1_Expressions_Alert/JS/main.js
@@ -0,0 +1,5 @@
+var A = "This is a string, \"and this is a quote with an escape.\"" + "I like to use concatenation, but don\'t like to use breaking"
++ " characters."
+var family = "Dad, Mom, and Sister" , dad = " Dad", mom = " Mom"
+document.write("My Family includes: ", family + dad + mom + '<br>')
+document.write(5*5)
